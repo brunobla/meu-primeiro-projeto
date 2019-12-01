93600105adb68634bc7bef346600e7f68c930b07
Bruno Abreu <brunoloureiro@localhost-2.local>
2019-12-01 16:55:15 -0300

    Integração com o travisCI

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/brunobla/meu-primeiro-projeto.svg?branch=master)](https://travis-ci.org/brunobla/meu-primeiro-projeto)
 # meu-primeiro-projeto
 Projeto para curso do Udemy sobre API REST
--- src/main/java/com/novaroma/sd/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/novaroma/sd/MeuPrimeiroProjetoApplication.java
@@ -9,8 +9,6 @@ public class MeuPrimeiroProjetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Ola minha primeira versão");
-		
-		
 	}
 
 }
