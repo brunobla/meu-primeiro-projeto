1c362fc2c75628083c220b6cc2782a8036e39dd6
Bruno Abreu <brunoloureiro@localhost-2.local>
2019-12-01 12:04:12 -0300

    Primeira Versão do Projeto

---
+++ src/test/java/com/novaroma/sd/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaroma.sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
